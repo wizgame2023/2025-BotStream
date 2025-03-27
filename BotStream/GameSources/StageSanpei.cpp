e8b1f4f5b708fcf2abdafb8bf0ee0a326a6dae17
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-03-27 16:11:03 +0900

    開発ステージの更新

--- BotStream/GameSources/StageSanpei.cpp
+++ BotStream/GameSources/StageSanpei.cpp
@@ -12,7 +12,7 @@ namespace basecross {
 	//	ゲームステージクラス実体
 	//--------------------------------------------------------------------------------------
 	void StageSanpei::CreateViewLight() {
-		const Vec3 eye(0.0f, 5.0f, -5.0f);
+		const Vec3 eye(0.0f, 15.0f, -15.0f);
 		const Vec3 at(0.0f);
 		auto PtrView = CreateView<SingleView>();
 		//ビューのカメラの設定
@@ -35,7 +35,14 @@ namespace basecross {
 		}
 		catch (...) {
 			throw;
-		}
+		}	
+		
+		//Player作成
+		auto player = AddGameObject<Player>(Vec3(0.0f,0.0f,0.0f),Vec3(0.0f,0.0f,0.0f),Vec3(1.0f,1.0f,1.0f));
+		SetSharedGameObject(L"Player", player);
+
+		//カメラマネージャ作成
+		AddGameObject<CameraManager>();
 	}
 
 }
