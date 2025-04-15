ef8a71b9b6622b5e24a74327441b52f89a2cd797
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-04-15 14:28:14 +0900

    ダッシュ回避処理

--- BotStream/GameSources/Player.h
+++ BotStream/GameSources/Player.h
@@ -16,10 +16,30 @@ namespace basecross{
 	private:
 		//ジャンプ力ぅ…
 		const float m_jumpPower = 18.0f;
+		//最高速
+		const float m_speedMax = 10;
+		//ダッシュ時の最高速
+		const float m_speedMaxDash = 20.0f;
+		//加速度
+		const float m_baseAccel = 400.0f;
+		//重力
+		const float m_gravity = -32.0f;
+		//落下時の終端速度
+		const float m_fallTerminal = -120.0f;
+		//摩擦係数(静/動/完全停止)
+		const float m_friction = .75f;
+		const float m_frictionDynamic = .5f;
+		const float m_frictionThreshold = .5f;
 		//スティックのデッドゾーン(あとでmanager系に置くかも)
 		float m_stickDeadZone = .1f;
 		//地上にいるか否か
 		bool m_isLand = false;
+		//回避したかのフラグ
+		bool m_dodgeFlag = false;
+		//回避時間を測る変数
+		float m_dodgeTime;
+		//ダッシュのフラグ
+		bool m_dashFlag = false;
 		//着地判定を無効化する時間
 		float m_disableLandDetect = 0.0f;
 
@@ -35,6 +55,12 @@ namespace basecross{
 		Vec3 GetMoveVector();
 
 		void Jump();
+
+		//ダッシュ処理
+		void Dash();
+
+		//回避処理
+		void Dodge();
 
 		// エフェクトの再生
 		void EfkPlaying(const wstring efkKey,const float rad, const Vec3 rotate);
