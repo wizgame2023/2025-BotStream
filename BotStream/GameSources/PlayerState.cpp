d4e833635d511c78a32763ae1dae9264f85d5912
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-04-16 09:59:34 +0900

    回避、徒歩、ダッシュの移動系ステートの作成

--- BotStream/GameSources/PlayerState.cpp
+++ BotStream/GameSources/PlayerState.cpp
@@ -7,6 +7,137 @@
 #include "Project.h"
 
 namespace basecross {
+	//Playerの歩くモーション
+	void PlayerWalkState::Enter()
+	{
+
+	}
+	void PlayerWalkState::Update(float deltaTime)
+	{
+		// 入力デバイス取得
+		auto inputDevice = App::GetApp()->GetInputDevice();
+		auto controller = inputDevice.GetControlerVec()[0];
+		Vec3 stick = Vec3(controller.fThumbLX, 0, controller.fThumbLY);
+		
+		//移動処理
+		Vec3 move = m_player->GetMoveVector();
+		m_player->PlayerMove(PlayerState_Walk);
+
+		//歩きアニメーション再生
+		if (move.length() != 0)
+		{
+			m_player->ChangeAnim(L"Walk");
+		}
+		else 
+		{
+			m_player->ChangeAnim(L"Idle");
+		}
+
+		//歩きステートのアニメーション再生
+		m_player->GetComponent<PNTBoneModelDraw>()->UpdateAnimation(deltaTime * 5);
+	
+		//回避ステートに変更する
+		if (controller.wPressedButtons & XINPUT_GAMEPAD_A)
+		{
+			m_player->ChangeState(L"Dodge");
+		}
+		////攻撃ステートに変更する 攻撃が実装されていないのでコメントアウト
+		//if (controller.wPressedButtons & XINPUT_GAMEPAD_X)
+		//{
+		//	m_player->ChangeState(L"Attack1");
+		//}
+
+	
+	}
+	void PlayerWalkState::Exit()
+	{
+
+	}
+
+	//回避ステート
+	void PlayerDodgeState::Enter()
+	{
+
+	}
+	void PlayerDodgeState::Update(float deltaTime)
+	{
+		// 入力デバイス取得
+		auto inputDevice = App::GetApp()->GetInputDevice();
+		m_controller = inputDevice.GetControlerVec()[0];
+
+		//移動処理
+		Vec3 move = m_player->GetMoveVector();
+		m_player->PlayerMove(PlayerState_Dodge);
+
+		bool dodgeFlag = m_player->GetDodgeFlag();
+		if (!dodgeFlag)
+		{
+			Exit(0);
+		}
+
+	}
+	void PlayerDodgeState::Exit(int a)//終了処理
+	{
+		//// 入力デバイス取得
+		//auto inputDevice = App::GetApp()->GetInputDevice();
+		//auto controller = inputDevice.GetControlerVec()[0];
+
+		//ダッシュステートにするか歩くステートにするか
+		if (m_controller.wButtons & XINPUT_GAMEPAD_A)
+		{
+			m_player->ChangeState(L"Dash");
+		}
+		else
+		{
+			m_player->ChangeState(L"PlayerWalk");
+		}
+
+	}
+
+	//ダッシュステート
+	void PlayerDashState::Enter()
+	{
+
+	}
+	void PlayerDashState::Update(float deltaTime)
+	{
+		// 入力デバイス取得
+		auto inputDevice = App::GetApp()->GetInputDevice();
+		auto controller = inputDevice.GetControlerVec()[0];
+
+		//移動処理
+		Vec3 move = m_player->GetMoveVector();
+		m_player->PlayerMove(PlayerState_Dash);
+
+		//歩きアニメーション再生
+		if (move.length() != 0)
+		{
+			m_player->ChangeAnim(L"Walk");
+		}
+		else
+		{
+			m_player->ChangeAnim(L"Idle");
+		}
+
+		//歩きステートのアニメーション再生
+		m_player->GetComponent<PNTBoneModelDraw>()->UpdateAnimation(deltaTime * 7);
+
+		//歩くステートに変更する
+		if (controller.wReleasedButtons & XINPUT_GAMEPAD_A)
+		{
+			m_player->ChangeState(L"PlayerWalk");
+		}
+		////攻撃ステートに変更する 攻撃が実装されていないのでコメントアウト
+		//if (controller.wPressedButtons & XINPUT_GAMEPAD_X)
+		//{
+		//	m_player->ChangeState(L"Attack1");
+		//}
+
+	}
+	void PlayerDashState::Exit()
+	{
+
+	}
 
 
 }
--- BotStream/GameSources/PlayerState.h
+++ BotStream/GameSources/PlayerState.h
@@ -5,10 +5,87 @@
 
 #pragma once
 #include "stdafx.h"
-#include "Project.h"
 
 namespace basecross {
+	class Player;
+	class PlayerWalkState :public StateBase
+	{
+	private:
+		shared_ptr<Player> m_player;
+	public:
+		PlayerWalkState(shared_ptr<GameObject>& obj) :
+			StateBase(obj),
+			m_player(dynamic_pointer_cast<Player>(obj))
+		{
+		}
 
+		~PlayerWalkState()
+		{
+		}
+
+		virtual void Enter();
+		virtual void Update(float deltaTime);
+		virtual void Exit();
+	};
+
+	//回避ステート
+	class PlayerDodgeState : public StateBase
+	{
+	private:
+		shared_ptr<Player> m_player;
+		CONTROLER_STATE m_controller;
+	public:
+		PlayerDodgeState(shared_ptr<GameObject>& obj) :
+			StateBase(obj),
+			m_player(dynamic_pointer_cast<Player>(obj))
+		{
+
+		}
+		~PlayerDodgeState()
+		{
+		}
+
+		virtual void Enter();
+		virtual void Update(float deltaTime);
+		virtual void Exit(int a);//なぜかExitで引数を入れないとエラー吐きます
+	};
+
+	//ダッシュステート
+	class PlayerDashState : public StateBase
+	{
+	private:
+		shared_ptr<Player> m_player;
+	public:
+		PlayerDashState(shared_ptr<GameObject>& obj) :
+			StateBase(obj),
+			m_player(dynamic_pointer_cast<Player>(obj))
+		{
+
+		}
+		~PlayerDashState()
+		{
+		}
+
+		virtual void Enter();
+		virtual void Update(float deltaTime);
+		virtual void Exit();
+	};
+
+	//プレイヤーステートマシン
+	class PlayerStateMachine : public StateMachineBase
+	{
+	public:
+		PlayerStateMachine(shared_ptr<GameObject>& obj)
+		{
+			//ステートの登録
+			AddState(L"PlayerWalk", shared_ptr<PlayerWalkState>(new PlayerWalkState(obj)));
+			AddState(L"Dodge", shared_ptr<PlayerDodgeState>(new PlayerDodgeState(obj)));
+			AddState(L"Dash", shared_ptr<PlayerDashState>(new PlayerDashState(obj)));
+
+			//最初のステートはWalkここからいろんなステートに変更する イーブイみたいなもの
+			ChangeState(L"PlayerWalk");
+		}
+	};
 
 }
 //end basecross
