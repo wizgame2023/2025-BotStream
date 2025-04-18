3b77714fec1c127333068608214212bfcc0b1ef0
Yuuta Sanpei <zeal23420155@fsgcl.jp>
2025-04-18 13:59:53 +0900

    ため攻撃実装と遠距離攻撃ステートの作成

--- BotStream/GameSources/PlayerState.cpp
+++ BotStream/GameSources/PlayerState.cpp
@@ -7,6 +7,41 @@
 #include "Project.h"
 
 namespace basecross {
+	void PlayerStateBase::Update(float deltaTime)
+	{
+
+		//コントローラーを受け取る
+		auto inputDevice = App::GetApp()->GetInputDevice();
+		m_controller = inputDevice.GetControlerVec()[0];
+
+		//カメラマネージャからロックオン対象の位置を取得する
+		shared_ptr<CameraManager> camera = nullptr;
+		camera = m_player->GetStage()->GetSharedGameObject<CameraManager>(L"CameraManager");
+		shared_ptr<Actor> targetObj = nullptr;
+		targetObj = camera->GetTargetObj();
+		//ターゲット対象がいる時対象との距離を測る
+		if (targetObj)
+		{
+			//Vec3 targetPos = Vec3(0.0f);
+			//Vec3 playerPos = Vec3(0.0f);
+			//targetPos = targetObj->GetComponent<Transform>()->GetPosition();
+			//playerPos = m_player->GetComponent<Transform>()->GetPosition();
+			////ロックオン対象との距離を計算する
+			m_targetDistance = camera->GetTargetDis();
+		}
+		else if (!targetObj)
+		{
+			m_targetDistance = 0.0f;
+		}
+
+
+	};
+	//ターゲット対象との距離を取得する
+	float PlayerStateBase::GetTargetDistance()
+	{
+		return m_targetDistance;
+	}
+
 	//Playerの歩くモーション
 	void PlayerWalkState::Enter()
 	{
@@ -39,14 +74,37 @@ namespace basecross {
 		{
 			m_player->ChangeState(L"Dodge");
 		}
-		//攻撃ステートに変更する 攻撃が実装されていないのでコメントアウト
-		if (m_controller.wPressedButtons & XINPUT_GAMEPAD_X)
+		//攻撃ステートに変更する　長押しだったら回転攻撃そうでなければ通常攻撃
+		//ロックオン対象の距離によって攻撃方法を変えるロックオンしてなければ近距離のみ
+		float meleeRange = 3.0f;
+		if (m_controller.wButtons & XINPUT_GAMEPAD_X)
 		{
-			m_player->ChangeState(L"Attack1");
-			m_player->AddEffect(PlayerEffect_Attack1);//攻撃エフェクトを出す
+			m_timeOfPushAttackButton += deltaTime;//押している時間を測る
+		}
+		if (m_controller.wReleasedButtons & XINPUT_GAMEPAD_X)
+		{
+			//攻撃する際距離によって攻撃方法を変える
+			if (m_targetDistance < meleeRange)
+			{//近距離
+				if (m_timeOfPushAttackButton >= 1.5f)
+				{
+					m_player->ChangeState(L"AttackEx");
+					m_player->AddEffect(PlayerEffect_AttackEx);//攻撃エフェクトを出す
+				}
+				if (m_timeOfPushAttackButton < 1.5f)
+				{
+					m_player->ChangeState(L"Attack1");
+					m_player->AddEffect(PlayerEffect_Attack1);//攻撃エフェクトを出す
+				}
+			}
+			else if (m_targetDistance >= meleeRange)
+			{//遠距離
+				m_player->ChangeState(L"AttackLong");
+				m_player->AddEffect(PlayerEffect_Beam);//攻撃エフェクトを出す
+			}
+
 		}
 
-	
 	}
 	void PlayerWalkState::Exit()
 	{
@@ -70,22 +128,23 @@ namespace basecross {
 		bool dodgeFlag = m_player->GetDodgeFlag();
 		if (!dodgeFlag)
 		{
-			Exit(0);
+			//ダッシュステートにするか歩くステートにするか
+			if (m_controller.wButtons & XINPUT_GAMEPAD_A)
+			{
+				m_player->ChangeState(L"Dash");
+			}
+			else
+			{
+				m_player->ChangeState(L"PlayerWalk");
+			}
 		}
 
 	}
-	void PlayerDodgeState::Exit(int a)//終了処理
+	void PlayerDodgeState::Exit()//終了処理
 	{
-		//ダッシュステートにするか歩くステートにするか
-		if (m_controller.wButtons & XINPUT_GAMEPAD_A)
-		{
-			m_player->ChangeState(L"Dash");
-		}
-		else
-		{
-			m_player->ChangeState(L"PlayerWalk");
-		}
+
 	}
+
 
 	//ダッシュステート
 	void PlayerDashState::Enter()
@@ -119,11 +178,23 @@ namespace basecross {
 		{
 			m_player->ChangeState(L"PlayerWalk");
 		}
-		//攻撃ステートに変更する 攻撃が実装されていないのでコメントアウト
-		if (m_controller.wPressedButtons & XINPUT_GAMEPAD_X)
+		//攻撃ステートに変更する
+		if (m_controller.wButtons & XINPUT_GAMEPAD_X)
 		{
-			m_player->ChangeState(L"Attack1");
-			m_player->AddEffect(PlayerEffect_Attack1);//攻撃エフェクトを出す
+			m_timeOfPushAttackButton += deltaTime;//押している時間を測る
+		}
+		if (m_controller.wReleasedButtons & XINPUT_GAMEPAD_X)
+		{
+			if (m_timeOfPushAttackButton >= 1.5f)
+			{
+				m_player->ChangeState(L"AttackEx");
+				m_player->AddEffect(PlayerEffect_AttackEx);//攻撃エフェクトを出す
+			}
+			if (m_timeOfPushAttackButton < 1.5f)
+			{
+				m_player->ChangeState(L"Attack1");
+				m_player->AddEffect(PlayerEffect_Attack1);//攻撃エフェクトを出す
+			}
 		}
 
 	}
@@ -143,80 +214,229 @@ namespace basecross {
 	}
 	void PlayerAttack1State::Update(float deltaTime)
 	{
+		// 入力デバイス取得
+		PlayerStateBase::Update(deltaTime);
+
 		//攻撃の時間計測
 		m_timeOfAttack += deltaTime;
-
 		//猶予時間以内に攻撃ボタンを押せたら次の攻撃ステートに遷移できる
 		if (m_timeOfAttack < m_graceTimeOfNextAttack)
 		{
+			//攻撃ステートに変更する
 			if (m_controller.wPressedButtons & XINPUT_GAMEPAD_X)
-			{
-				m_nestAttackFlag = true;
-				m_player->AddEffect(PlayerEffect_Attack2);//攻撃エフェクトを出す
+			{			
+				m_nestAttackFlag = true;	
 			}
 		}
 		//次の攻撃に移行できない状態なら攻撃判定の色を変える(デバック用)
 		if (m_timeOfAttack >= m_graceTimeOfNextAttack)
 		{
-			m_AttackObj->GetComponent<PNTStaticDraw>()->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 1.0f));
-			m_AttackObj->GetComponent<PNTStaticDraw>()->SetEmissive(Col4(1.0f, 0.0f, 0.0f, 1.0f));
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 0.0f));
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetEmissive(Col4(1.0f, 0.0f, 0.0f, 0.0f));
 		}
 		//攻撃の時間を越えたら別のステートに移動する
 		if (m_timeOfAttack >= m_timeMaxOfAttack)
-		{
-			Exit(0);
+		{	
+			auto stage = m_player->GetStage();
+
+			//ステート遷移
+			if (m_nestAttackFlag)
+			{
+				m_player->ChangeState(L"Attack2");////次の攻撃ステートに移動
+				m_player->AddEffect(PlayerEffect_Attack2);//攻撃エフェクトを出す
+			}
+			else
+			{
+				stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+				m_player->ChangeState(L"PlayerWalk");
+			}
 		}
 		
 	}
-	void PlayerAttack1State::Exit(int a)
+	void PlayerAttack1State::Exit()
 	{
 		auto stage = m_player->GetStage();
-
-		//ステート遷移
-		if (m_nestAttackFlag)
-		{
-			m_player->ChangeState(L"Attack2");//まだAttack2ステート作成してない			
-		}
-		else
-		{
-			m_timeOfAttack = 0.0f;//リセット
-			stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
-			m_player->ChangeState(L"PlayerWalk");
-		}
+		stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+		m_timeOfAttack = 0.0f;//リセット
+		m_nestAttackFlag = false;
 	}
 
 
 	//攻撃ステート(２番目に出る攻撃)
 	void PlayerAttack2State::Enter()
 	{
-
+		//攻撃の当たり判定を出す
+		auto stage = m_player->GetStage();
+		//攻撃の当たり判定(仮)
+		m_AttackObj = stage->AddGameObject<Cube>(Vec3(0.0f, 2.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f),Col4(0.0f,0.0f,1.0f,1.0f));
 	}
 	void PlayerAttack2State::Update(float deltaTime)
 	{
+		// 入力デバイス取得
+		PlayerStateBase::Update(deltaTime);
+
 		//攻撃の時間計測
 		m_timeOfAttack += deltaTime;
 
 		//猶予時間以内に攻撃ボタンを押せたら次の攻撃ステートに遷移できる
 		if (m_timeOfAttack < m_graceTimeOfNextAttack)
 		{
-			//m_nestAttackFlag = true;
+			//攻撃ステートに変更する
+			if (m_controller.wPressedButtons & XINPUT_GAMEPAD_X)
+			{
+				m_nestAttackFlag = true;
+			}
 		}
 		//次の攻撃に移行できない状態なら攻撃判定の色を変える(デバック用)
 		if (m_timeOfAttack >= m_graceTimeOfNextAttack)
 		{
-			m_AttackObj->GetComponent<PNTStaticDraw>()->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 1.0f));
-			m_AttackObj->GetComponent<PNTStaticDraw>()->SetEmissive(Col4(1.0f, 0.0f, 0.0f, 1.0f));
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 0.0f));
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetEmissive(Col4(1.0f, 0.0f, 0.0f, 0.0f));
 		}
 		//攻撃の時間を越えたら別のステートに移動する
 		if (m_timeOfAttack >= m_timeMaxOfAttack)
 		{
-			Exit(0);
+			auto stage = m_player->GetStage();
+			m_timeOfAttack = 0.0f;//リセット
+
+			//ステート遷移
+			if (m_nestAttackFlag)
+			{
+				stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+				m_player->ChangeState(L"Attack3");//次の攻撃ステートに移動
+				m_player->AddEffect(PlayerEffect_Attack3);//攻撃エフェクトを出す
+			}
+			else
+			{
+				stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+				m_player->ChangeState(L"PlayerWalk");
+			}
 		}
 	}
-	void PlayerAttack2State::Exit(int a)
+	void PlayerAttack2State::Exit()
 	{
-		m_player->ChangeState(L"PlayerWalk");
+		auto stage = m_player->GetStage();
+		stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+		m_timeOfAttack = 0.0f;//リセット
+		m_nestAttackFlag = false;
 	}
+
+
+	//攻撃ステート(3番目に出る攻撃)
+	void PlayerAttack3State::Enter()
+	{
+		//攻撃の当たり判定を出す
+		auto stage = m_player->GetStage();
+		//攻撃の当たり判定(仮)
+		m_AttackObj = stage->AddGameObject<Cube>(Vec3(0.0f, 2.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), Col4(1.0f, 0.0f, 1.0f, 1.0f));
+	}
+	void PlayerAttack3State::Update(float deltaTime)
+	{
+		// 入力デバイス取得
+		PlayerStateBase::Update(deltaTime);
+
+		//攻撃の時間計測
+		m_timeOfAttack += deltaTime;
+
+		//猶予時間以内に攻撃ボタンを押せたら次の攻撃ステートに遷移できる
+		if (m_timeOfAttack < m_graceTimeOfNextAttack)
+		{
+			//攻撃ステートに変更する
+			if (m_controller.wPressedButtons & XINPUT_GAMEPAD_X)
+			{
+				m_nestAttackFlag = true;
+			}
+		}
+		//次の攻撃に移行できない状態なら攻撃判定の色を変える(デバック用)
+		if (m_timeOfAttack >= m_graceTimeOfNextAttack)
+		{
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetDiffuse(Col4(1.0f, 0.0f, 0.0f, 0.0f));
+			m_AttackObj->GetComponent<PNTStaticDraw>()->SetEmissive(Col4(1.0f, 0.0f, 0.0f, 0.0f));
+		}
+		//攻撃の時間を越えたら別のステートに移動する
+		if (m_timeOfAttack >= m_timeMaxOfAttack)
+		{
+			auto stage = m_player->GetStage();
+			m_timeOfAttack = 0.0f;//リセット
+
+			//ステート遷移
+			if (m_nestAttackFlag)
+			{
+				stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+				m_player->ChangeState(L"AttackEx");//次の攻撃ステートに移動
+				m_player->AddEffect(PlayerEffect_AttackEx);//攻撃エフェクトを出す
+			}
+			else
+			{
+				stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+				m_player->ChangeState(L"PlayerWalk");
+			}
+		}
+	}
+	void PlayerAttack3State::Exit()
+	{
+		auto stage = m_player->GetStage();
+		stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+		m_timeOfAttack = 0.0f;//リセット
+		m_nestAttackFlag = false;
+	}
+
+
+	//攻撃ステート(3番目に出る攻撃)
+	void PlayerAttackExState::Enter()
+	{
+		//攻撃の当たり判定を出す
+		auto stage = m_player->GetStage();
+		//攻撃の当たり判定(仮)
+		m_AttackObj = stage->AddGameObject<Cube>(Vec3(0.0f, 2.0f, 0.0f), Vec3(0.0f, 0.0f, 0.0f), Vec3(1.0f, 1.0f, 1.0f), Col4(0.0f, 0.0f, 0.0f, 1.0f));
+	}
+	void PlayerAttackExState::Update(float deltaTime)
+	{
+		//攻撃の時間計測
+		m_timeOfAttack += deltaTime;
+
+		//攻撃の時間を越えたら別のステートに移動する
+		if (m_timeOfAttack >= m_timeMaxOfAttack)
+		{
+			auto stage = m_player->GetStage();
+			m_timeOfAttack = 0.0f;//リセット
+
+			m_player->ChangeState(L"PlayerWalk");
+		}
+	}
+	void PlayerAttackExState::Exit()
+	{
+		auto stage = m_player->GetStage();
+		stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+		m_timeOfAttack = 0.0f;//リセット
+	}
+
+
+	//void PlayerAttackLongState::Enter()
+	//{
+	//	//球を出す
+	//}
+	//void PlayerAttackLongState::Update(float deltaTime)
+	//{
+	//	//攻撃の時間計測
+	//	m_timeOfAttack += deltaTime;
+
+	//	//攻撃の時間を越えたら別のステートに移動する
+	//	if (m_timeOfAttack >= m_timeMaxOfAttack)
+	//	{
+	//		auto stage = m_player->GetStage();
+	//		m_timeOfAttack = 0.0f;//リセット
+
+	//		m_player->ChangeState(L"PlayerWalk");
+	//	}
+
+	//}
+	//void PlayerAttackLongState::Exit()
+	//{
+	//	auto stage = m_player->GetStage();
+	//	stage->RemoveGameObject<Cube>(m_AttackObj);//攻撃判定削除
+	//	m_timeOfAttack = 0.0f;//リセット
+	//}
 
 }
 //end basecross
--- BotStream/GameSources/PlayerState.h
+++ BotStream/GameSources/PlayerState.h
@@ -7,7 +7,8 @@
 #include "stdafx.h"
 
 namespace basecross {
-	class Player;
+	class Player;	
+	class Actor;
 	class Cube;
 	//プレイヤーステートの元となるクラス
 	class PlayerStateBase :public StateBase
@@ -15,6 +16,9 @@ namespace basecross {
 	protected:
 		shared_ptr<Player> m_player;//プレイヤーの実体
 		CONTROLER_STATE m_controller;//コントローラー
+		float m_timeOfPushAttackButton = 0.0f;//攻撃ボタンを押している時間
+		shared_ptr<Actor> m_targetObj = nullptr;//ロックオン時の対象
+		float m_targetDistance;//ターゲット対象との距離
 	public:
 		PlayerStateBase(shared_ptr<GameObject>& obj) :
 			StateBase(obj),
@@ -28,13 +32,10 @@ namespace basecross {
 
 		
 		virtual void Enter() {};
-		virtual void Update(float deltaTime) 
-		{
-			//コントローラーを受け取る
-			auto inputDevice = App::GetApp()->GetInputDevice();
-			m_controller = inputDevice.GetControlerVec()[0];
-		};
+		virtual void Update(float deltaTime);
 		virtual void Exit() {};
+
+		float GetTargetDistance();
 	};
 
 	//徒歩ステート
@@ -56,6 +57,7 @@ namespace basecross {
 		virtual void Enter();
 		virtual void Update(float deltaTime);
 		virtual void Exit();
+
 	};
 
 	//回避ステート
@@ -74,7 +76,8 @@ namespace basecross {
 
 		virtual void Enter();
 		virtual void Update(float deltaTime);
-		virtual void Exit(int a);//なぜかExitで引数を入れないとエラー吐きます
+		virtual void Exit();
+
 	};
 
 	//ダッシュステート
@@ -82,6 +85,7 @@ namespace basecross {
 	{
 	private:
 		shared_ptr<Player> m_player;
+		float m_timeOfPushAttackButton = 0.0f;//攻撃ボタンを押している時間
 	public:
 		PlayerDashState(shared_ptr<GameObject>& obj) :
 			PlayerStateBase(obj),
@@ -103,11 +107,11 @@ namespace basecross {
 	{
 	private:
 		//攻撃時間
-		float m_timeMaxOfAttack = 1.0f;
+		float m_timeMaxOfAttack = 0.7f;
 		//攻撃時間計測
 		float m_timeOfAttack;
 		//次の攻撃の猶予時間
-		float m_graceTimeOfNextAttack = 0.7f;
+		float m_graceTimeOfNextAttack = 0.5f;
 		//次の攻撃をするかのフラグ
 		float m_nestAttackFlag = false;
 
@@ -125,7 +129,7 @@ namespace basecross {
 
 		virtual void Enter();
 		virtual void Update(float deltaTime);
-		virtual void Exit(int a);
+		virtual void Exit();
 	};
 
 	//攻撃ステート(２番目に出る攻撃)
@@ -133,11 +137,11 @@ namespace basecross {
 	{
 	private:
 		//攻撃時間
-		float m_timeMaxOfAttack = 1.0f;
+		float m_timeMaxOfAttack = 0.7f;
 		//攻撃時間計測
 		float m_timeOfAttack;
 		//次の攻撃の猶予時間
-		float m_graceTimeOfNextAttack = 0.7f;
+		float m_graceTimeOfNextAttack = 0.5f;
 		//次の攻撃をするかのフラグ
 		float m_nestAttackFlag = false;
 
@@ -155,9 +159,98 @@ namespace basecross {
 
 		virtual void Enter();
 		virtual void Update(float deltaTime);
-		virtual void Exit(int a);
+		virtual void Exit();
 	};
 
+	//攻撃ステート(3番目に出る攻撃)
+	class PlayerAttack3State :public PlayerStateBase
+	{
+	private:
+		//攻撃時間
+		float m_timeMaxOfAttack = 1.0f;
+		//攻撃時間計測
+		float m_timeOfAttack;
+		//次の攻撃の猶予時間
+		float m_graceTimeOfNextAttack = 0.9f;
+		//次の攻撃をするかのフラグ
+		float m_nestAttackFlag = false;
+
+		shared_ptr<Cube> m_AttackObj = nullptr;
+
+	public:
+		PlayerAttack3State(shared_ptr<GameObject>& obj) :
+			PlayerStateBase(obj)
+		{
+
+		}
+		~PlayerAttack3State()
+		{
+		}
+
+		virtual void Enter();
+		virtual void Update(float deltaTime);
+		virtual void Exit();
+	};
+
+	//攻撃ステート(最後に出る攻撃)
+	class PlayerAttackExState :public PlayerStateBase
+	{
+	private:
+		//攻撃時間
+		float m_timeMaxOfAttack = 1.2f;
+		//攻撃時間計測
+		float m_timeOfAttack;
+		////次の攻撃の猶予時間
+		//float m_graceTimeOfNextAttack = 0.9f;
+		////次の攻撃をするかのフラグ
+		//float m_nestAttackFlag = false;
+
+		shared_ptr<Cube> m_AttackObj = nullptr;
+
+	public:
+		PlayerAttackExState(shared_ptr<GameObject>& obj) :
+			PlayerStateBase(obj)
+		{
+
+		}
+		~PlayerAttackExState()
+		{
+		}
+
+		virtual void Enter();
+		virtual void Update(float deltaTime);
+		virtual void Exit();
+	};
+
+	////攻撃ステート(最後に出る攻撃)
+	//class PlayerAttackLongState :public PlayerStateBase
+	//{
+	//private:
+	//	//攻撃時間
+	//	float m_timeMaxOfAttack = 1.2f;
+	//	//攻撃時間計測
+	//	float m_timeOfAttack;
+	//	////次の攻撃の猶予時間
+	//	//float m_graceTimeOfNextAttack = 0.9f;
+	//	////次の攻撃をするかのフラグ
+	//	//float m_nestAttackFlag = false;
+
+	//	shared_ptr<Cube> m_AttackObj = nullptr;
+
+	//public:
+	//	PlayerAttackLongState(shared_ptr<GameObject>& obj) :
+	//		PlayerStateBase(obj)
+	//	{
+
+	//	}
+	//	~PlayerAttackLongState()
+	//	{
+	//	}
+
+	//	virtual void Enter();
+	//	virtual void Update(float deltaTime);
+	//	virtual void Exit();
+	//};
 
 	//プレイヤーステートマシン
 	class PlayerStateMachine : public StateMachineBase
@@ -170,10 +263,15 @@ namespace basecross {
 			AddState(L"Dodge", shared_ptr<PlayerDodgeState>(new PlayerDodgeState(obj)));
 			AddState(L"Dash", shared_ptr<PlayerDashState>(new PlayerDashState(obj)));
 			AddState(L"Attack1", shared_ptr<PlayerAttack1State>(new PlayerAttack1State(obj)));
+			AddState(L"Attack2", shared_ptr<PlayerAttack2State>(new PlayerAttack2State(obj)));
+			AddState(L"Attack3", shared_ptr<PlayerAttack3State>(new PlayerAttack3State(obj)));
+			AddState(L"AttackEx", shared_ptr<PlayerAttackExState>(new PlayerAttackExState(obj)));
+			//AddState(L"AttackLong", shared_ptr<PlayerAttackLongState>(new PlayerAttackLongState(obj)));
 
 			//最初のステートはWalkここからいろんなステートに変更する イーブイみたいなもの
 			ChangeState(L"PlayerWalk");
 		}
+		
 	};
 
 }
