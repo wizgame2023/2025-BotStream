aa3187447d12e465f29e79fcc5507ec11604b717
Kaito Sato <zeal23420035@fsgcl.jp>
2025-05-09 14:34:06 +0900

    エフェクトの大きさを調整できるようにした

--- BotStream/GameSources/Actor.h
+++ BotStream/GameSources/Actor.h
@@ -96,8 +96,9 @@ namespace basecross {
 		}
 
 		// エフェクトの再生
-		void EfkPlaying(const wstring efkKey, const float rad, const Vec3 rotate, Vec3 pushPos = Vec3(0.0f));
+		void EfkPlaying(const wstring efkKey, const float rad, const Vec3 rotate, const Vec3 scale = Vec3(1.0f), Vec3 pushPos = Vec3(0.0f));
 		void EfkPlaying(const wstring efkKey, const float rad, const Vec3 rotate, Col4 changeColor, Vec3 pushPos = Vec3(0.0f));
+		//void EfkPlaying(const wstring efkKey, const float rad, const Vec3 rotate, const Vec3 scale = Vec3(1.0f), Vec3 pushPos = Vec3(0.0f));
 		// 地面着地
 		void OnLanding();
 
